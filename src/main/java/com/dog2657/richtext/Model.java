32032e4597a8cccbbd87650c6ef3fc42b3a994c8
Dog2657 <reeceya2006@gmail.com>
2024-04-11 09:16:49 +0100

    Fixed: cursor offset

--- src/main/java/com/dog2657/richtext/Model.java
+++ src/main/java/com/dog2657/richtext/Model.java
@@ -7,7 +7,7 @@ public class Model {
     private String fileLocation;
     private char[] buffer = new char[bufferSize];
 
-    private int cursor = 3;
+    private int cursor = 0;
 
     private static Model instance;
     private Viewer viewer;
