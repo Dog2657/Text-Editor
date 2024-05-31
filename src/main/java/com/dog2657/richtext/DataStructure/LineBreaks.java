d56885054f0e9b3cf147e0ef0aa403fc03462c17
Dog2657 <reeceya2006@gmail.com>
2024-05-31 01:00:56 +0100

    Displaying cursor on lines correctly

--- src/main/java/com/dog2657/richtext/DataStructure/LineBreaks.java
+++ src/main/java/com/dog2657/richtext/DataStructure/LineBreaks.java
@@ -14,6 +14,9 @@ public class LineBreaks {
 
     private LineBreaks(){}
 
+    /**
+     * Goes through the text to find all line breaks
+     */
     public void parse(String text){
         breaks.clear();
         char[] content = text.toCharArray();
@@ -25,14 +28,26 @@ public class LineBreaks {
         }
     }
 
-    public int getLine(int cursor){
+    /*
+    * Gets the line that a position is on
+     */
+    public int getLine(int position){
         for (int i=0; i<breaks.size(); i++) {
-            if(breaks.get(i) < cursor)
+            if(breaks.get(i) < position)
                 continue;
             return i;
         }
         return 0;
     }
+
+    static public int getRelativeLocation(int position, int line){
+        int lastBreak = LineBreaks.getInstance().getBreaks().get(line -1);
+        return position - (lastBreak + 1);
+    }
+
+    static public int getRelativeLocation(int position){
+        int line = LineBreaks.getInstance().getLine(position);
+        return getRelativeLocation(position, line);
 
     public ArrayList<Integer> getBreaks() {
         return breaks;
--- src/main/java/com/dog2657/richtext/Viewer.java
+++ src/main/java/com/dog2657/richtext/Viewer.java
@@ -1,28 +1,27 @@
 package com.dog2657.richtext;
 
 import com.dog2657.richtext.DataStructure.LineBreaks;
-import com.dog2657.richtext.DataStructure.Piece;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 import javafx.scene.text.Text;
 
-import java.util.ArrayList;
-import java.util.LinkedList;
-
 
 public class Viewer extends Canvas {
     int fontSize;
     double fontWidth;
+    double lineGap;
     Font font;
 
     public Viewer(int width, int height){
         super(width, height);
 
         fontSize = 15;
+        lineGap = 1;
         font = new Font("Andale Mono", fontSize);
         fontWidth = getFontWidth(font);
+
 
         this.render();
     }
@@ -37,12 +36,21 @@ public class Viewer extends Canvas {
         gc.setFont(font);
         gc.setFill(Color.WHITESMOKE);
 
-        gc.fillText(Model.getInstance().get_text_output(), 0, 15);
+        String text = Model.getInstance().get_text_output();
+        String[] lines = text.split("\n");
+
+        double lineGap = fontSize + this.lineGap;
+
+        for (int i=0; i<lines.length; i++) {
+            gc.fillText(lines[i], 0, (i * lineGap) + 15);
+        }
+
+        //gc.fillText(text, 0, 15);
 
         gc.setStroke(Color.RED);
 
         Location cursorLoc = getCursorLocation();
-        gc.strokeLine(cursorLoc.x, cursorLoc.y, cursorLoc.x, cursorLoc.y + fontSize);
+        gc.strokeLine(cursorLoc.x, cursorLoc.y + 5, cursorLoc.x, cursorLoc.y + fontSize);
     }
 
     public void update(){
@@ -62,11 +70,11 @@ public class Viewer extends Canvas {
         double fontWidth = getFontWidth(font);
 
         if(line > 0){
-            int lastBreak = LineBreaks.getInstance().getBreaks().get(line -1);
-            loc -= lastBreak + 1;
+            //int lastBreak = LineBreaks.getInstance().getBreaks().get(line -1);
+            loc = LineBreaks.getRelativeLocation(loc);
         }
 
-        return new Location(fontWidth * loc, line * fontSize);
+        return new Location(fontWidth * loc, line * (fontSize + lineGap));
     }
 
 
