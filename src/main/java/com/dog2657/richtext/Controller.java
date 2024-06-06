6a71305f624f98c6b353fddb8925483aec925b91
Dog2657 <reeceya2006@gmail.com>
2024-06-06 18:30:10 +0100

    Moving vertically working

--- src/main/java/com/dog2657/richtext/Controller.java
+++ src/main/java/com/dog2657/richtext/Controller.java
@@ -1,6 +1,7 @@
 package com.dog2657.richtext;
 
 import java.io.IOException;
+import java.util.ArrayList;
 
 public abstract class Controller {
     public static void openFile(String path){
@@ -29,13 +30,45 @@ public abstract class Controller {
         Model.getInstance().moveCursor(1);
     }
 
-    public static void moveCursorLeft(int moves){
-        Model.getInstance().moveCursor(moves * -1);
+    public static void moveCursorLeft(){
+        Model.getInstance().moveCursor(-1);
     }
 
-    public static void moveCursorRight(int moves){
-        Model.getInstance().moveCursor(moves);
+    public static void moveCursorRight(){
+        Model.getInstance().moveCursor(1);
     }
+
+    public static void moveCursorUp(){
+        ArrayList<Integer> breaks = Model.getInstance().getBreaks();
+        int currentLine = Model.getInstance().get_cursor_line();
+        if(currentLine <= 0)
+            return;
+
+        int currentRelativeLoc = Model.getInstance().get_cursor_relative_location();
+        int previousLineLength = (currentLine <= 1)? breaks.get(currentLine -1):breaks.get(currentLine -1) - breaks.get(currentLine -2);
+
+        if(currentRelativeLoc >= previousLineLength)
+            Model.getInstance().setCursor( breaks.get(currentLine - 1) );
+        else
+            Model.getInstance().setCursor(
+                    (currentLine <= 1)? currentRelativeLoc : breaks.get(currentLine -2) - currentRelativeLoc
+            );
+    }
+
+    public static void moveCursorDown(){
+        ArrayList<Integer> breaks = Model.getInstance().getBreaks();
+        int currentLine = Model.getInstance().get_cursor_line();
+        if((breaks.size() -1) <= currentLine)
+            return;
+
+        int currentRelativeLoc = Model.getInstance().get_cursor_relative_location();
+
+        if(currentRelativeLoc >= (breaks.get(currentLine + 1) - breaks.get(currentLine)))
+            Model.getInstance().setCursor( breaks.get(currentLine + 1) );
+        else
+            Model.getInstance().setCursor(breaks.get(currentLine) + currentRelativeLoc + 1);
+    }
+
 
     public static void delete(boolean forwards) {
         int relativeLoc = Model.getInstance().get_cursor_relative_location();
