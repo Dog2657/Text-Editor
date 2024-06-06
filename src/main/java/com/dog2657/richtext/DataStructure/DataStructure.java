a1c8f5577f79fbbf85d734f19a1c1c005048aedf
Dog2657 <reeceya2006@gmail.com>
2024-06-06 22:08:50 +0100

    Implemented DRY practice

--- src/main/java/com/dog2657/richtext/DataStructure/DataStructure.java
+++ src/main/java/com/dog2657/richtext/DataStructure/DataStructure.java
@@ -1,5 +1,6 @@
 package com.dog2657.richtext.DataStructure;
 
+import java.util.ArrayList;
 import java.util.LinkedList;
 
 public class DataStructure {
@@ -31,25 +32,9 @@ public class DataStructure {
     }
 
 
+
     public void add_text(int cursor, String text){
-        //The location in all pieces
-        int absolute_location = 0;
-
-        Piece selected = null;
-        int selected_index = 0;
-
-        for (int i=0; i<this.pieces.size(); i++){
-            selected = this.pieces.get(i);
-            if(absolute_location + selected.getLength() >= cursor) {
-                selected_index = i;
-                break;
-            }
-
-            absolute_location += selected.getLength();
-        }
-
-        //The position within the selected data Piece
-        int relative_location = cursor - absolute_location;
+        piece_processing_info info = get_piece_processing_info(cursor);
 
         //The start of the new text in add source
         int start = this.add.length();
@@ -58,60 +43,43 @@ public class DataStructure {
         //The new data Piece
         Piece data = new Piece(start, text.length(), Sources.add);
 
-        if(relative_location <= 0) {//Is before selected piece
-            this.pieces.add(selected_index, data);
+        if(info.relative_location <= 0) {//Is before selected piece
+            this.pieces.add(info.selected_index, data);
 
-        }else if (relative_location >= selected.getLength()) {//Is after selected piece
+        }else if (info.relative_location >= info.selected.getLength()) {//Is after selected piece
 
             //Checks if it can be combined with selected when source is add (For key presses)
-            if(selected.getSource() == Sources.add && (this.add.length() - selected.getLength() - 1) == selected.getStart()){
-                selected.setLength(selected.getLength() + 1);
+            if(info.selected.getSource() == Sources.add && (this.add.length() - info.selected.getLength() - 1) == info.selected.getStart()){
+                info.selected.setLength(info.selected.getLength() + 1);
             }else{
-                this.pieces.add(selected_index + 1, data);
+                this.pieces.add(info.selected_index + 1, data);
             }
 
         }else{//Is inside selected piece
-            Piece ending_piece = selected.split(relative_location);
-            this.pieces.add(selected_index + 1, data);
-            this.pieces.add(selected_index + 2, ending_piece);
+            Piece ending_piece = info.selected.split(info.relative_location);
+            this.pieces.add(info.selected_index + 1, data);
+            this.pieces.add(info.selected_index + 2, ending_piece);
         }
 
         length += text.length();
     }
 
     public void delete_text(int cursor){
-        //The location in all pieces
-        int absolute_location = 0;
+        piece_processing_info info = get_piece_processing_info(cursor);
 
-        Piece selected = null;
-        int selected_index = 0;
-
-        for (int i=0; i<this.pieces.size(); i++){
-            selected = this.pieces.get(i);
-            if(absolute_location + selected.getLength() >= cursor) {
-                selected_index = i;
-                break;
-            }
-
-            absolute_location += selected.getLength();
-        }
-
-        //The position within the selected data Piece
-        int relative_location = cursor - absolute_location;
-
-        if(selected_index <= 0 && relative_location <= 0)
+        if(info.selected_index <= 0 && info.relative_location <= 0)
             return;
 
         final int moves = 1;
 
-        if(relative_location <= 0) {
-            selected.setStart(selected.getStart() + moves);
-        }else if (relative_location >= selected.getLength()) {//Is after selected piece
-            selected.setLength(selected.getLength() - moves);
+        if(info.relative_location <= 0) {
+            info.selected.setStart(info.selected.getStart() + moves);
+        }else if (info.relative_location >= info.selected.getLength()) {//Is after selected piece
+            info.selected.setLength(info.selected.getLength() - moves);
         }else{//Is inside selected piece
-            Piece ending_piece = selected.split(relative_location);
-            selected.setLength(selected.getLength() - moves);
-            this.pieces.add(selected_index + 1, ending_piece);
+            Piece ending_piece = info.selected.split(info.relative_location);
+            info.selected.setLength(info.selected.getLength() - moves);
+            this.pieces.add(info.selected_index + 1, ending_piece);
         }
 
         this.length -= moves;
@@ -125,4 +93,50 @@ public class DataStructure {
     public int getLength() {
         return length;
     }
+
+
+
+    /* ---------------------------- DRY Code below  ---------------------------- */
+
+    private class piece_processing_info{
+        public int absolute_location;
+        public int relative_location;
+        public int selected_index;
+        public Piece selected;
+
+
+        private piece_processing_info(int absolute_location, int relative_location, int selected_index, Piece selected) {
+            this.absolute_location = absolute_location;
+            this.relative_location = relative_location;
+            this.selected_index = selected_index;
+            this.selected = selected;
+        }
+
+    }
+
+    private piece_processing_info get_piece_processing_info(int cursor){
+        //The location in all pieces
+        int absolute_location = 0;
+
+        Piece selected = null;
+        int selected_index = 0;
+
+        for (int i=0; i<pieces.size(); i++){
+            selected = pieces.get(i);
+            if(absolute_location + selected.getLength() >= cursor) {
+                selected_index = i;
+                break;
+            }
+
+            absolute_location += selected.getLength();
+        }
+
+        //The position within the selected data Piece
+        int relative_location = cursor - absolute_location;
+
+        return new piece_processing_info(absolute_location, relative_location, selected_index, selected);
+    }
+
+
+
 }
